c3ee3356338ce3de34f8e76e616d166964f79efa
TheMilkMan <131654331+TheMilkMan6669@users.noreply.github.com>
2025-07-24 04:04:38 -0800

    Hotfix for arrow key no-scroll script
    I somehow included the spacebar as an input to be cancelled out here, which was preventing you from putting spaces between words when a text field shows up. That is fixed here.

--- src/scripts/no-arrow-key-scroll.js
+++ src/scripts/no-arrow-key-scroll.js
@@ -1,5 +1,5 @@
 window.addEventListener('keydown', function(e) {
-    if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', ' '].includes(e.key)) {
+    if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) {
         e.preventDefault();
     }
 }, true);
