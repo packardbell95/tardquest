f1c59ae2ee8a29845e8ce560374b4b9f40002afb
MySpace Tom <tom@packardbell95.com>
2026-04-30 14:02:33 -0500

    Avoiding inclusion of unnecessary properties in GameMap.#seeFrom()

--- src/scripts/Map.js
+++ src/scripts/Map.js
@@ -1521,11 +1521,7 @@ class GameMap {
             ? Object.entries(this.#entities).map(e => {
                 const o = e[1];
                 // @TODO It should not be necessary to set IDs here
-                if (! o.id) {
-                    o.id = e[0];
-                }
-
-                return o;
+                return { id: o.id || e[0], x: o.x, y: o.y };
             })
             : [];
 
